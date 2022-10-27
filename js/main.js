f0477f0a90532014185945983b5b1585c312ad5c
luciobo <luciobo@icloud.com>
2022-10-27 18:15:56 +0200

    correzione errori

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 
 const userAge = prompt("inserisci qui la tuia eta")
+let userAgeNumber = parseInt(userAge)
 
 console.log(userAge)
 
-const userChilometers = prompt("quanti chilometri vuoi percorrere")
+const userChilometers = parseInt(prompt("quanti chilometri vuoi percorrere"))
 
 console.log(userChilometers)
 
-var prezzoBiglietto = (userChilometers * 0,21)
+var prezzoBiglietto = (userChilometers * 0.21)
 
 console.log(prezzoBiglietto)
 
-if ( isNaN( userAge ) ) {
-    alert( "Il numero inserito non è valido" );
+console.log(typeof userAge, typeof userAgeNumber, userAge, userAgeNumber)
+
+
+if (userAgeNumber < 18) {
+    var prezzoBigliettoScontato = prezzoBiglietto - (prezzoBiglietto * 20 / 100)
+    console.log(prezzoBigliettoScontato)
+} else if (userAgeNumber > 65) {
+    var prezzoBigliettoScontato = prezzoBiglietto - (prezzoBiglietto * 60 / 100)
+    console.log(prezzoBigliettoScontato)
+} else {
+    var prezzoBiglietto = (userChilometers * 0.21)
 }
 
-if (userAge) {
-    
-}
+
+
+
+
+/* 
+1 se l'user age number ha meno di 18 anni applico lo sconto di 20 %
+2 
+Formula percentuale = prezzo - (prezzo * sconto / 100)
+*/
