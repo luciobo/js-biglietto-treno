c7784359abd056dfa0b3236c3b672639c361a16d
luciobo <luciobo@icloud.com>
2022-10-27 13:41:08 +0200

    corretti errori semantici in conbsole log

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 const userAge = prompt("inserisci qui la tuia eta")
 
-console.log("userAge")
+console.log(userAge)
 
 const userChilmeters = prompt("quanti chilometri vuoi percorrere")
 
-console.log("userChilmeters")
+console.log(userChilmeters)
