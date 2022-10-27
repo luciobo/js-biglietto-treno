8fb1e06ef73a632a37b117d016036d3e7b94aada
luciobo <luciobo@icloud.com>
2022-10-27 16:24:44 +0200

    qualche progresoo

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ const userAge = prompt("inserisci qui la tuia eta")
 
 console.log(userAge)
 
-const userChilmeters = prompt("quanti chilometri vuoi percorrere")
+const userChilometers = prompt("quanti chilometri vuoi percorrere")
 
-console.log(userChilmeters)
+console.log(userChilometers)
+
+var prezzoBiglietto = (userChilometers * 0,21)
+
+console.log(prezzoBiglietto)
+
+if ( isNaN( userAge ) ) {
+    alert( "Il numero inserito non è valido" );
+}
+
+if (userAge) {
+    
+}
