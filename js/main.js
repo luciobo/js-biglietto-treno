7929472c2c0a565c4f932b938db43b9c3d1e69ef
luciobo <luciobo@icloud.com>
2022-10-27 13:36:51 +0200

    inzio modifiche js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const userAge = prompt("inserisci qui la tuia eta")
+
+console.log("userAge")
+
+const userChilmeters = prompt("quanti chilometri vuoi percorrere")
+
+console.log("userChilmeters")
